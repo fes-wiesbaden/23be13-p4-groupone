4df0a1a9d7b9d6f28a3ceb665d7421004881bf29
Noah Bach <noah.j.bach@gmail.com>
2025-09-03 12:50:15 +0200

    feat: added Course class in Model

---
+++ backend/src/main/java/com/gradesave/backend/models/Course.java
@@ -0,0 +1,25 @@
+package com.gradesave.backend.models;
+
+import jakarta.persistence.*;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+//import java.util.HashSet;
+//import java.util.Set;
+
+@Entity
+@Table(name="course")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String courseName;
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public String getCourseName() { return courseName; }
+    
+    public void setCourseName(String courseName) { this.courseName = courseName; }
+}
