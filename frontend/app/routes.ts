5188ef2289691688191b53f78aa76f225439d0bd
Daniel Hess <159422453+dhess-dev@users.noreply.github.com>
2025-09-04 10:48:34 +0200

    feat: add user management system with backend and frontend integrations (#24)
    * feat: add user management system with backend and frontend integrations
    
    * feat: enhance user creation form with API config and success/error alerts
    
    * feat: add validation annotations to User model and improve error handling in UserController
    
    * feat: improve error handling and validation in UserController, refactor UserService exceptions

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -2,5 +2,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     index("routes/home.tsx"),
+    route("user", "routes/user.tsx"),
     // route("test", "routes/test.tsx"), add routes like this
 ] satisfies RouteConfig;
