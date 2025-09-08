22a89d5f87a8c36be8b7d001ea728a5ba02f0c4f
Noah Bach <noah.j.bach@gmail.com>
2025-09-08 08:04:53 +0200

    feat: made classes for course repository

---
+++ backend/src/main/java/com/gradesave/backend/services/CourseService.java
@@ -0,0 +1,8 @@
+package com.gradesave.backend.services;
+
+import com.gradesave.backend.models.Course;
+import java.util.UUID;
+
+public class CourseService implements CrudService<Course, UUID> {
+
+}
