9f884d7c3cdac55db2371bb5d1d44ad52cfbe56b
Noah Bach <noah.j.bach@gmail.com>
2025-09-10 10:55:53 +0200

    feat: Klassen löschen Knopf funktioniert

--- frontend/app/adminfunctions.tsx
+++ frontend/app/adminfunctions.tsx
@@ -18,14 +18,25 @@ export async function postNewTestCourseEntry () {
     try {
         // POST for new course
         const response = await fetch(`${API_CONFIG.BASE_URL}/api/klassen`, {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: '{"courseName":"test-name"}',
-              });
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: '{"courseName":"test-name"}',
+        });
     }
     catch (e) {
+        console.error("Request failed: ", e);
+    }
+}
+
+export async function deleteCourse (id) {
+    console.log("Pressed. Id: ", id);
+    try {
+        const response = await fetch(`${API_CONFIG.BASE_URL}/api/klassen/${id}`, {
+            method: 'DELETE',
+        });
+    } catch (e) {
         console.error("Request failed: ", e);
     }
 }
