27a3a956fcbafc36f79f432f0c3656074934ca1b
derEchteNoah <noah.j.bach@gmail.com>
2025-12-08 08:49:22 +0100

    Fix/csv UI updating (#86)
    * fix: Tabelle lädt neu wenn Datei hochgeladen
    
    * fix: Datei wird deselcted & Bug gefixt
    
    - Datei wird deselected wenn sie hochgeladen wird
    - Bug gefixt, bei dem das result des POST requests versucht wurde, zu JSON umzuwandeln. Damit wurde nie etwas gemacht, außer  Fehler zu verursachen
    
    * fix: falsche Snackbar angezeigt beim erstellen des benutzers
    
    * feat: Snackbar nach Dateihochladen
    
    * chore: fixed typos
    
    ---------
    
    Co-authored-by: Noah Bach <noah.bach@sinc.de>

--- frontend/app/routes/userOverview.tsx
+++ frontend/app/routes/userOverview.tsx
@@ -6,8 +6,12 @@
  * @Edited by Kebba Ceesay
  * @Date: 03/12/2025
  * Snackbar integration completed
+ * 
+ * @Edited by Noah Bach
+ * @Date: 05/12/2025
+ * Better usability for CSV import
+ * (Snackbar, remove file from form on submit, reload on success)
  */
-import * as React from "react";
 import {
   Dialog,
   DialogTitle,
@@ -204,7 +208,6 @@ export default function UsersPage() {
       });
 
       if (!res.ok) {
-        // alert("Aktualisieren fehlgeschlagen.");
         console.error("Fehler beim Aktualisieren des Benutzers:", res.statusText);
 
         setSnackbarMessage(`Fehler beim Bearbeiten! Code: ${res.status}`);
@@ -232,8 +235,7 @@ export default function UsersPage() {
         credentials: "include",
         body: JSON.stringify(createRequest),
       });
-      if (res.ok) {
-        // alert("Erstellen fehlgeschlagen.");
+      if (!res.ok) {
         console.error("Fehler beim Erstellen des Benutzers:", res.statusText);
 
         setSnackbarMessage(`Fehler beim Erstellen! Code: ${res.status}`);
@@ -256,7 +258,8 @@ export default function UsersPage() {
         type={CsvType.USERS}
         url={`${API_CONFIG.BASE_URL}/api/csv/upload`}
         upload_name={"Hochladen der CSV"}
-        select_name={"Wählen sie eine Nutzer CSV aus"}
+        select_name={"Wählen Sie eine Nutzer-CSV aus"}
+        doAfterUpload={load}
       />
       <DataGridWithAdd<UserRow>
         columns={columns}
