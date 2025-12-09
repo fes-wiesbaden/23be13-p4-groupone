d2fd10fd511069b6900e2f4bc902c51dcd032e40
kebba.ceesay <kebba.ceesay@invensity.com>
2025-12-09 19:08:35 +0100

    added the size import

--- backend/src/main/java/com/gradesave/backend/models/Project.java
+++ backend/src/main/java/com/gradesave/backend/models/Project.java
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 /**
  * @author: Michael Holl
